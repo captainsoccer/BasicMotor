ee680fc6c220a8c3896cd463e02ceda2d6075676
captainsoccer <justeyallevi@gmail.com>
2025-08-20 17:36:46 +0300

    moved to version 3.2.2-beta

--- example_projects/swerve_example/src/main/java/frc/robot/subsystems/drivetrain/swerveModule/SwerveModuleConstants.java
+++ example_projects/swerve_example/src/main/java/frc/robot/subsystems/drivetrain/swerveModule/SwerveModuleConstants.java
@@ -27,60 +27,60 @@ public enum SwerveModuleConstants {
             // PID gains for the drive motor
             3, new PIDGains(5, 0, 0),
             // The feed forwards for the drive motor (the setpoint feed forward is the kV)
-            new FeedForwardsGains(2.7),
-            0.011902948,
+            new FeedForwardsGains(2.7, 0.109),
+            0.0023431,
 
             // PID gains for the steer motor
-            4, new PIDGains(20, 25, 0, 0.05, 4, 0.001),
+            4, new PIDGains(8, 0, 0, 0.05, 4, 0.001),
             //The feed forwards for the steer motor
-            new FeedForwardsGains(0, 0.35464),
+            new FeedForwardsGains(0, 0),
             //The motor constants used in the simulation
-            0.1,0.2,
+            0.1,0.02,
             new Translation2d(0.29, 0.29)), //The translation of the modules relative to the center of the robot
 
     FRONT_RIGHT(5, 0,
             // PID gains for the drive motor
             6, new PIDGains(5, 0, 0),
             // The feed forwards for the drive motor (the setpoint feed forward is the kV)
-            new FeedForwardsGains(2.7),
-            0.011902948,
+            new FeedForwardsGains(2.7, 0.109),
+            0.023431,
 
             // PID gains for the steer motor
-            7, new PIDGains(20, 25, 0, 0.05, 4, 0.001),
+            7, new PIDGains(8, 0, 1, 0.05, 4, 0.001),
             //The feed forwards for the steer motor
-            new FeedForwardsGains(0, 0.35464),
+            new FeedForwardsGains(0, 0),
             //The motor constants used in the simulation
-            0.1,0.2,
+            0.1,0.02,
             new Translation2d(0.29, -0.29)),
 
     BACK_LEFT(8, 0,
             // PID gains for the drive motor
             9, new PIDGains(5, 0, 0),
             // The feed forwards for the drive motor (the setpoint feed forward is the kV)
-            new FeedForwardsGains(2.7),
-            0.011902948,
+            new FeedForwardsGains(2.7, 0.109),
+            0.23431,
 
             // PID gains for the steer motor
-            10, new PIDGains(20, 25, 0, 0.05, 4, 0.001),
+            10, new PIDGains(8, 0, 1, 0.05, 4, 0.001),
             //The feed forwards for the steer motor
-            new FeedForwardsGains(0, 0.35464),
+            new FeedForwardsGains(0, 0),
             //The motor constants used in the simulation
-            0.1,0.2,
+            0.1,0.02,
             new Translation2d(-0.29, 0.29)),
 
     BACK_RIGHT(11, 0,
             // PID gains for the drive motor
             12, new PIDGains(5, 0, 0),
             // The feed forwards for the drive motor (the setpoint feed forward is the kV)
-            new FeedForwardsGains(2.7),
-            0.011902948,
+            new FeedForwardsGains(2.7, 0.109),
+            0.023431,
 
             // PID gains for the steer motor
-            13, new PIDGains(20, 25, 0, 0.05, 4, 0.001),
+            13, new PIDGains(8, 0, 0, 0.05, 4, 0.001),
             //The feed forwards for the steer motor
-            new FeedForwardsGains(0, 0.35464),
+            new FeedForwardsGains(0, 0),
             //The motor constants used in the simulation
-            0.1,0.2,
+            0.1,0.02,
             new Translation2d(-0.29, -0.29));
 
     // Drive motor constants
@@ -164,8 +164,8 @@ public enum SwerveModuleConstants {
         DRIVE_MOTOR_CONFIG.motorConfig.id = driveMotorID;
         DRIVE_MOTOR_CONFIG.pidConfig = PIDConfig.fromGains(drivePIDGains);
         DRIVE_MOTOR_CONFIG.feedForwardConfig = FeedForwardConfig.fromFeedForwards(driveFeedForwards);
-        DRIVE_MOTOR_CONFIG.simulationConfig.kV = driveFeedForwards.getSetpointFeedForward();
-        DRIVE_MOTOR_CONFIG.simulationConfig.kA = driveKA;
+        DRIVE_MOTOR_CONFIG.simulationConfig.kV = 0.13208;
+        DRIVE_MOTOR_CONFIG.simulationConfig.kA = 0.013716;
 
         //apply specific configurations for the steer motor
         STEER_MOTOR_CONFIG.motorConfig.name = NAME + " Steer Motor";

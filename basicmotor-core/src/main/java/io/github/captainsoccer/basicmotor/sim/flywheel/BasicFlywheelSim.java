b4c4ce77b559c32b864abec6152ec8568c40a1d2
captainsoccer <justeyallevi@gmail.com>
2025-08-20 19:41:51 +0300

    added simulation fix for flywheel like in motor sim

--- basicmotor-core/src/main/java/io/github/captainsoccer/basicmotor/sim/flywheel/BasicFlywheelSim.java
+++ basicmotor-core/src/main/java/io/github/captainsoccer/basicmotor/sim/flywheel/BasicFlywheelSim.java
@@ -1,7 +1,14 @@
 package io.github.captainsoccer.basicmotor.sim.flywheel;
 
 
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.AngularAccelerationUnit;
+import edu.wpi.first.units.AngularVelocityUnit;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.VoltageUnit;
+import edu.wpi.first.units.measure.Per;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import io.github.captainsoccer.basicmotor.BasicMotorConfig;
 import io.github.captainsoccer.basicmotor.gains.ControllerGains;
@@ -76,8 +83,18 @@ public class BasicFlywheelSim extends BasicSimSystem {
                     config.motorConfig.motorType,
                     simConfig.velocityStandardDeviation);
         } else {
+            double unitConversion = config.motorConfig.unitConversion;
+
+            Per<VoltageUnit, AngularVelocityUnit> kV =
+                    Units.Volts.per(Units.RotationsPerSecond).ofNative(simConfig.kV * unitConversion);
+
+            Per<VoltageUnit, AngularAccelerationUnit> kA =
+                    Units.Volts.per(Units.RotationsPerSecondPerSecond).ofNative(simConfig.kA * unitConversion);
+
+            LinearSystem<N1, N1, N1> plant = LinearSystemId.identifyVelocitySystem(kV.in(Units.VoltsPerRadianPerSecond), kA.in(Units.VoltsPerRadianPerSecondSquared));
+
             return new FlywheelSim(
-                    LinearSystemId.identifyVelocitySystem(simConfig.kV, simConfig.kA),
+                    plant,
                     config.motorConfig.motorType,
                     simConfig.velocityStandardDeviation);
 

c86733e371744892dc6138fd172988aba518e09b
captainsoccer <justeyallevi@gmail.com>
2025-10-17 13:46:57 +0300

    added javadoc for error handler

--- basicmotor-core/src/main/java/io/github/captainsoccer/basicmotor/errorHandling/ErrorHandler.java
+++ basicmotor-core/src/main/java/io/github/captainsoccer/basicmotor/errorHandling/ErrorHandler.java
@@ -5,18 +5,31 @@ import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 /**
- *
+ * Handles error and warning for each motor.
+ * Uses {@link MessageHandler} to store and update messages.
  */
 public class ErrorHandler {
+    /** The message handler for errors */
     private final MessageHandler errorHandler = new MessageHandler();
+    /** The message handler for warnings */
     private final MessageHandler warningHandler = new MessageHandler();
 
+    /** The error log frame used for logging */
     private final ErrorLogFrame frame = new ErrorLogFrame();
 
+    /**
+     * Logs an error message
+     * @param msg the error message to log (automatically adds timestamp and brackets)
+     */
     public void logError(String msg) {
         errorHandler.addMessage(msg);
     }
 
+    /**
+     * Logs an error message and optionally prints the stack trace
+     * @param msg the error message to log (automatically adds timestamp and brackets)
+     * @param printStackTrace whether to print the stack trace to the driver station
+     */
     public void logError(String msg, boolean printStackTrace){
         logError(msg);
 
@@ -24,10 +37,19 @@ public class ErrorHandler {
             DriverStation.reportError(msg, true);
     }
 
+    /**
+     * Logs a warning message
+     * @param msg the warning message to log (automatically adds timestamp and brackets)
+     */
     public void logWarning(String msg) {
         warningHandler.addMessage(msg);
     }
 
+    /**
+     * Logs a warning message and optionally prints the stack trace
+     * @param msg the warning message to log (automatically adds timestamp and brackets)
+     * @param printStackTrace whether to print the stack trace to the driver station
+     */
     public void logWarning(String msg, boolean printStackTrace){
         logWarning(msg);
 
@@ -35,6 +57,7 @@ public class ErrorHandler {
             DriverStation.reportWarning(msg, true);
     }
 
+    /** Updates the error and warning messages */
     private String getErrors() {
         if(errorHandler.isEmpty())
             return "";
@@ -43,6 +66,7 @@ public class ErrorHandler {
         return errorHandler.getMessages();
     }
 
+    /** Updates the warning messages */
     private String getWarnings() {
         if(warningHandler.isEmpty())
             return "";
@@ -51,12 +75,17 @@ public class ErrorHandler {
         return warningHandler.getMessages();
     }
 
+    /** Gets the error log frame for logging */
     public ErrorLogFrame getErrorFrame() {
         frame.errors = getErrors();
         frame.warnings = getWarnings();
         return frame;
     }
 
+    /**
+     * The log frame for errors and warnings.
+     * Used for logging.
+     */
     public static class ErrorLogFrame implements LoggableInputs, Cloneable {
         public String errors = "";
         public String warnings = "";
